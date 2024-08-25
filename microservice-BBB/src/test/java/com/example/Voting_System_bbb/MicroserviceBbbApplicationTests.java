bb27c61ed4b0b192eb166f19ac89316adf02903d
Inocencio Cordeiro <cordeiroturorias@gmail.com>
2024-08-25 11:43:15 +0100

    change the struture in the project

---
+++ microservice-BBB/src/test/java/com/example/Voting_System_bbb/MicroserviceBbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Voting_System_bbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
